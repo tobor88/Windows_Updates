df50f0a613739d0ad41cfb14baace5f35b870b1f
tobor <rosborne@osbornepro.com>
2022-04-11 12:53:14 -0600

    Create FixStuckWindowsUpdate.ps1

---
+++ FixStuckWindowsUpdate.ps1
@@ -0,0 +1,21 @@
+Write-Output "[*] Stopping services that use the directories we need renamed"
+Stop-Service -Name wuauserv,cryptsvc,bits,msiserver
+
+Write-Output "[*] Renaming C:\Windows\SoftwareDistribution to SoftwareDistribution.bak"
+If (Test-Path -Path "C:\Windows\SoftwareDistribution.bak") {
+
+    Remove-Item -Path "C:\Windows\SoftwareDistribution.bak" -Force
+
+}  # End If
+Rename-Item -Path "C:\Windows\SoftwareDistribution" -NewName "C:\Windows\SoftwareDistribution.bak"
+
+Write-Output "[*] Renaming C:\Windows\System32\catroot2 to catroot2.bak"
+If (Test-Path -Path "C:\Windows\System32\catroot2.bak") {
+
+    Remove-Item -Path "C:\Windows\System32\catroot2.bak" -Force
+
+}  # End If
+Rename-Item -Path "C:\Windows\System32\catroot2" -NewName "C:\Windows\System32\catroot2.bak"
+
+Write-Output "[*] A restart is required to load the changes. Update Windows After the restart"
+Restart-Computer -Confirm
