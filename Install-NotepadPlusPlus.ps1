aabd82f56cd5cce0ccf2429d4b3c418991829468
tobor <rosborne@osbornepro.com>
2023-04-25 12:18:14 -0600

    Update Install-NotepadPlusPlus.ps1

--- Install-NotepadPlusPlus.ps1
+++ Install-NotepadPlusPlus.ps1
@@ -87,14 +87,14 @@ System.Management.Automation.PSObject
     }  # End If
 
     $Uri = 'https://api.github.com/repos/notepad-plus-plus/notepad-plus-plus/releases/latest'
-    $UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+    $UserAgent = [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox
 
     Write-Verbose -Message "[v] $(Get-Date -Format 'MM-dd-yyyy hh:mm:ss') Downloading Notepad++ from GitHub"
     Try {
         
-        $GetLinks = Invoke-RestMethod -Uri $Uri -Method GET -UseBasicParsing -UserAgent $UserAgent -ContentType 'application/json; charset=utf-8'
+        $GetLinks = Invoke-RestMethod -Uri $Uri -Method GET -UseBasicParsing -UserAgent $UserAgent -ContentType 'application/json; charset=utf-8' -Verbose:$False
         $DownloadLink = ($GetLinks.assets | Where-Object -Property Name -like "npp.*.Installer.x64.exe").browser_download_url
-        $DResponse = Invoke-WebRequest -Uri $DownloadLink -UseBasicParsing -UserAgent $UserAgent -OutFile $OutFile
+        Invoke-WebRequest -Uri $DownloadLink -UseBasicParsing -UserAgent $UserAgent -OutFile $OutFile -Verbose:$False | Out-Null
  
     } Catch {
  
