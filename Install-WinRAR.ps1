09aa1972af0318e8c55df2a1e23f99e2d1dade86
tobor <rosborne@osbornepro.com>
2023-06-20 12:45:37 -0600

    Update Install-WinRAR.ps1

--- Install-WinRAR.ps1
+++ Install-WinRAR.ps1
@@ -63,17 +63,19 @@ System.Management.Automation.PSObject
         param(
             [Parameter(
                 Position=0,
-                Mandatory=$False,
-                ValueFromPipeline=$False)]  # End Parameter
+                Mandatory=$False
+            )]  # End Parameter
             [ValidateScript({$_ -like "*.exe"})]
             [String]$OutFile = "C:\Windows\Temp\winrar-x64-version.exe",
  
             [Parameter(
-                Mandatory=$False)]  # End Parameter
+                Mandatory=$False
+            )]  # End Parameter
             [Switch]$DownloadOnly,
  
             [Parameter(
-                Mandatory=$False)]  # End Parameter
+                Mandatory=$False
+            )]  # End Parameter
             [Switch]$TryTLSv13
         )  # End param
  
