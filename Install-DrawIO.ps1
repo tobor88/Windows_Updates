14b412fa980b94de3fccb483630d62cddd687117
tobor <rosborne@osbornepro.com>
2023-06-20 12:42:27 -0600

    Update Install-DrawIO.ps1

--- Install-DrawIO.ps1
+++ Install-DrawIO.ps1
@@ -63,17 +63,19 @@ System.Management.Automation.PSObject
         param(
             [Parameter(
                 Position=0,
-                Mandatory=$False,
-                ValueFromPipeline=$False)]  # End Parameter
+                Mandatory=$False
+            )]  # End Parameter
             [ValidateScript({$_ -like "*.exe"})]
             [String]$OutFile = "$env:TEMP\draw.io-version-windows-installer.exe",
  
             [Parameter(
-                Mandatory=$False)]  # End Parameter
+                Mandatory=$False
+            )]  # End Parameter
             [Switch]$DownloadOnly,
 
             [Parameter(
-                Mandatory=$False)]  # End Parameter
+                Mandatory=$False
+            )]  # End Parameter
             [Switch]$TryTLSv13
         )   # End param
  
